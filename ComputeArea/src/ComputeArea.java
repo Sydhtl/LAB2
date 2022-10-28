200a6564595dcf4e547376d636eeaae399099432
saidahtul <saidahtul@LAPTOP-NVTLMBU4>
2022-10-28 10:23:25 +0800

    lab2

---
+++ ComputeArea/src/ComputeArea.java
@@ -0,0 +1,21 @@
+
+public class ComputeArea {
+
+	public static void main(String[] args) {
+		double radius = 50;
+		double pi ;
+		double area;
+		 
+		
+		
+		pi = 3.14159 ;
+		
+		area= radius * radius * pi;
+		
+		System.out.println("The area of the circle of the radius " + radius + " is " + area );
+		
+		
+
+	}
+
+}
